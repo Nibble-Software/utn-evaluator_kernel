7d7e1fca59c29ba9c417ba6d5df333e3b4ab2346
chelo154 <chelo154@gmail.com>
2020-10-08 11:52:31 -0300

    Refactorized Nibble_Evaluator

--- NibbleEvaluator/domain/tester.py
+++ NibbleEvaluator/domain/tester.py
@@ -1,13 +1,26 @@
+from domain.file_reader import FileReader
+from domain.runners.cpprunner import CppRunner
+from domain.code_test_status import TestStatus
 
 class Tester():
 
-    def __init__(self,expected_output,real_output):
-        self.expected_output = expected_output
-        self.real_output = real_output
+    def __init__(self,path_evaluated_file,path_inputs_file,path_expected_outputs_file):
+
+        program_data = program_data = FileReader.read_files(path_inputs_file,path_expected_outputs_file)
+        self.runner = CppRunner(path_evaluated_file,program_data['inputs'])
+        self.expected_output = program_data['outputs']
 
     def test_code(self):
-        print (self.expected_output)
-        print (self.real_output)    
-        return self.expected_output == self.real_output
+        if self.expected_output == self.real_output:
+            return TestStatus.TEST_PASSED
+        else:
+            return TestStatus.TEST_FAILED
+
+    def run_test(self):
+        self.real_output = self.runner.run_progam()
+        status = self.test_code()
+        return status
+
+
 
     pass
--- NibbleEvaluator/nibble_evaluator.py
+++ NibbleEvaluator/nibble_evaluator.py
@@ -2,27 +2,15 @@ import os
 import subprocess
 from sys import argv
 import domain.code_test_status
-from domain.runners.cpprunner import CppRunner
 from domain.tester import Tester
-from domain.code_test_status import TestStatus
-from domain.file_reader import FileReader
+
 
 def test_file(path_evaluated_file,path_inputs,path_expected_outputs):
 
-    program_data = FileReader.read_files(path_inputs,path_expected_outputs)
+    tester = Tester(path_evaluated_file,path_inputs,path_expected_outputs)
+    status = tester.run_test()
+    return status
 
-    cpp_runner = CppRunner(path_evaluated_file,program_data['inputs'])
-
-    resultado = cpp_runner.run_progam()
-
-    tester = Tester(program_data['outputs'],resultado)
-
-    test_passed = tester.test_code()
-
-    if test_passed:
-        return TestStatus.TEST_PASSED;
-    else:
-        return TestStatus.TEST_FAILED;
 
 
 
