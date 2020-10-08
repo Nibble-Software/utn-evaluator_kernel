34da09769c49181165e6f5e13a263d0b95333c03
chelo154 <chelo154@gmail.com>
2020-10-08 11:56:11 -0300

    Updated Tester class

--- NibbleEvaluator/domain/tester.py
+++ NibbleEvaluator/domain/tester.py
@@ -6,19 +6,28 @@ class Tester():
 
     def __init__(self,path_evaluated_file,path_inputs_file,path_expected_outputs_file):
 
-        program_data = program_data = FileReader.read_files(path_inputs_file,path_expected_outputs_file)
+        program_data = FileReader.read_files(path_inputs_file,path_expected_outputs_file)
+
         self.runner = CppRunner(path_evaluated_file,program_data['inputs'])
+
         self.expected_output = program_data['outputs']
 
+
     def test_code(self):
+
         if self.expected_output == self.real_output:
             return TestStatus.TEST_PASSED
+
         else:
             return TestStatus.TEST_FAILED
 
+
     def run_test(self):
+
         self.real_output = self.runner.run_progam()
+
         status = self.test_code()
+
         return status
 
 
